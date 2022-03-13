6c6f50ea455a5435cf36ff0ed6a9544bdddf7a76
swap2896 <kalra.swapnill2896@gmail.com>
2022-03-13 21:29:39 +0530

    modified files for heroku

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/rock-paper-scissors'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/rock-paper-scissors/'}),
